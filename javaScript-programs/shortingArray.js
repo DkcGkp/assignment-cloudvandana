08b362997dbf3585de0b8b4333748a49b1a58a73
DkcGkp <devendragkp1998@gmail.com>
2023-10-22 22:30:06 +0530

    Shorting Array Fixed

--- javaScript-programs/shortingArray.js
+++ javaScript-programs/shortingArray.js
@@ -1,14 +1,25 @@
-// Sample array of numbers
-const numbers = [5, 2, 9, 1, 5, 6, 3, 12, 19];
+function descendingBubbleSort(arr) {
+    let len = arr.length; // Get the length of the array
+    let swapped; // Initialize a variable to track if any swaps are made
 
-// Use the `sort` method with a custom comparison function
-numbers.sort(function(a, b) {
-    // To sort in descending order, subtract `b` from `a`.
-    // If the result is negative, `a` comes before `b`.
-    // If the result is positive, `b` comes before `a`.
-    // If the result is 0, the order remains unchanged.
-    return b - a;
-});
+    do {
+        swapped = false; // Reset the "swapped" flag to false
 
-// Display the sorted array in descending order
-console.log("Sorted in descending order:", numbers);
+        for (let i = 0; i < len - 1; i++) {
+            if (arr[i] < arr[i + 1]) { // Compare adjacent elements
+                // Swap elements if they are in the wrong order (ascending)
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true; // Set the "swapped" flag to true
+            }
+        }
+    } while (swapped); // Continue looping until no more swaps are made
+
+    return arr; // Return the sorted array in descending order
+}
+
+const myArray = [5, 2, 9, 1, 5, 6, 8, 15, 0];
+descendingBubbleSort(myArray);
+console.log(myArray); // Outputs: [5, 2, 9, 1, 5, 6, 8, 15, 0]
+
